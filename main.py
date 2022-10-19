ad75ec9c51242b2e5ddce18ab174aefa81940865
p7raneeth <praneethkrishna27@hotmail.com>
2022-10-19 17:20:36 +0530

    added data validation

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 # import tensorflow
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from distutils import extension
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_validate, StratifiedKFold, train_test_split
+from sklearn.feature_extraction import *
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import eli5
+from fastapi import *
 
-if __name__ == '__main__':
-    print("Project Started")
-    print('testing github pipeline')
-    # Define Sequential model with 3 layers
-    model = keras.Sequential(
-        [
-            layers.Dense(2, activation="relu", name="layer1"),
-            layers.Dense(3, activation="relu", name="layer2"),
-            layers.Dense(4, name="layer3"),
-        ]
-    )
-    # Call model on a test input
-    x = tf.ones((3, 3))
-    y = model(x)
+from data import read_validate_data
 
-    print(model)
+
+app_nlp = FastAPI()
+
+
+
+@app_nlp.get('/')
+async def sanity_check():
+    return {'message': 'Hello World!'}
+
+
+@app_nlp.post('/data_validation')
+
+def data_validation(fname: UploadFile=File(None)):
+    extension = fname.filename.split('.')[-1]
+    flag = read_validate_data(fname, extension)
+    print("validation finished")
+    return {'message': f'data_validation is completed successfully with flag {validation_dict[str(flag)]}'}
+
+validation_dict = {'0': 'Auto and Custom',
+                    '1': 'Invalid',
+                   '2': 'Custom only'}
